e96131b49831816483303ff01f54e1af931352ba
menezessilvamaria-svg <menezes.silva.maria@escola.pr.gov.br>
2026-05-27 11:40:57 -0300

    Add files via upload

---
+++ aula html/script.js
@@ -0,0 +1,15 @@
+const body = document.body;
+body.style.margin= '0'; 
+
+//========== CABEÇALHO ==========
+const cabecalho = document.create Element ('h1');
+cabecalho.textContent= 'Criando elementos com JavaScript';
+
+cabecalho.style.color = '#ffffff';
+cabecalho.style.fontSize = '40px';
+cabecalho.style.textAlign = 'center';
+cabecalho.style.display = 'flex';
+cabecalho.style.alignItems = 'center';
+cabecalho.style.justifyContent = 'center';
+cabecalho.style.backgroundColor = '#3a3281';
+
